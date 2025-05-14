5128a24d77fe4eb3c96769df7096d6b5705fe3db
Serapheim Dimitropoulos <serapheim.dimitropoulos@weka.io>
2025-05-13 20:38:13 -0400

    Fix: linter incorrect args for RemoteBackend

--- lmcache/experimental/storage_backend/__init__.py
+++ lmcache/experimental/storage_backend/__init__.py
@@ -76,7 +76,7 @@ def CreateStorageBackends(
                                             dst_device)
         else:
             remote_backend = RemoteBackend(config, metadata, loop,
-                                           memory_allocator, dst_device,
+                                           local_cpu_backend, dst_device,
                                            lookup_server)
         backend_name = str(remote_backend)
         storage_backends[backend_name] = remote_backend
